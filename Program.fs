80803a6b12c5bd793b7e40bf31d402acda6e5f4d
ElOraiby <wael.eloraiby@gmail.com>
2015-07-01 16:46:51 -0400

    add raw level ast and tests

--- Program.fs
+++ Program.fs
@@ -1,7 +1,44 @@
-﻿open System
+﻿//
+// Lam Reference Compiler
+// Copyright (C) 2015  Wael El Oraiby
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+open System
+open Microsoft.FSharp.Text.Lexing
+
+let (|>!) (a: 'A) (b: 'A -> 'B) = b a |> ignore; a
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+    if argv.Length = 1 then
+        if IO.File.Exists argv.[0] then
+            let content = IO.File.ReadAllText argv.[0]
+            let lexbuf = LexBuffer<char>.FromString content
+            try
+                Parser.start (Lexer.read argv.[0] content) lexbuf
+                |> printfn "%A"
 
+                0 // return an integer exit code
+            with e ->
+                printfn "%s" e.Message
+                printfn "   @line %d : %d" (lexbuf.StartPos.Line + 1) (lexbuf.StartPos.Column + 1)
+                3
+        else
+            printfn "Error: file %s doesn't exist" argv.[0]
+            2
+    else
+        printfn "Error -> usage: lam filename"
+        1
---
+++ RawAst.fs
@@ -0,0 +1,78 @@
+﻿//
+// Lam Reference Compiler
+// Copyright (C) 2015  Wael El Oraiby
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+module RawAst
+
+open System
+
+exception SyntaxError of string
+
+type TokenInfo = {
+    FileName    : string
+    Line        : int
+    Column      : int
+    Offset      : int
+}
+with
+    static member New(file: string, lineNumber: int, column: int, offset: int) = {
+        FileName    = file
+        Line        = lineNumber
+        Column      = column
+        Offset      = offset
+    }
+
+    static member Empty = {
+        FileName    = ""
+        Line        = 0
+        Column      = 0
+        Offset      = 0
+    }
+
+type Identifier = string * TokenInfo
+type Path       = Identifier list
+
+type Type       =
+    | Path      of Path
+    | Function  of FuncType
+    | Array     of Type
+
+and FuncType    = Type * (Field list)
+
+and Field      = {
+    Identifier  : Identifier
+    Type        : Type
+}
+
+type ConstBool      = bool * TokenInfo
+type ConstChar      = char * TokenInfo
+type ConstSInt64    = Int64 * TokenInfo
+type ConstReal64    = double * TokenInfo
+type ConstString    = string * TokenInfo
+
+type Node =
+    | Alias     of Path * Path
+    | Open      of Path
+    | Struct    of Identifier * (Field list)
+    | Record    of Identifier * (Field list)
+    | Union     of Identifier * (Field list)
+    | Function  of Identifier * FuncType
+    | ConstBool     of ConstBool
+    | ConstChar     of ConstChar
+    | ConstString   of ConstString
+    | ConstSInt64   of ConstSInt64
+    | ConstReal64   of ConstReal64
+    | Module    of Identifier * Node list
